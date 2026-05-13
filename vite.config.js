0c69337c72fad77a8e5abe10bc00f7372fcad975
Jake <wisejakey@gmail.com>
2026-05-12 21:46:42 -0600

    portfolio

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dans: resolve(__dirname, 'projects/dans.html'),
+        catsMeow: resolve(__dirname, 'projects/cats-meow.html'),
+        strikeNight: resolve(__dirname, 'projects/strike-night.html'),
+      },
+    },
+  },
+  plugins: [tailwindcss()],
+})
